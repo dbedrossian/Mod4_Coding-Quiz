0e3f18aaeac2a2397f39a5e5995c81d94517b558
dbedrossian <danny.bedrossian@gmail.com>
2022-11-15 20:58:34 -0500

    Added questions

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+var question1 = {
+    question: "Which instrument is NOT a member of the brass family?",
+    answers: ["Trumpet", "Tuba", "Trombone", "English Horn"],
+    correctAnswer: "English Horn",
+}
+
+var question2 = {
+    question: "Which alto-clef string instrument is considered the middle of the string family?",
+    answers: ["Violin", "Cello", "Viola", "Bass"],
+    correctAnswer: "Viola",
+}
+
+var question3 = {
+    question: "Which instrument is considered percussion?",
+    answers: ["Plucked Cello", "Muted Trumpet", "Electric Violin", "Sustained Piano"],
+    correctAnswer: "Sustained Piano",
+}
+
+var question4 = {
+    question: "Approximately how many musicians make up a standard modern orchestra?",
+    answers: ["35", "60", "100", "150"],
+    correctAnswer: "100",
+}
+
+var question5 = {
+    question: "Which instrument is NOT a double reed?",
+    answers: ["Oboe", "Clarinet", "Bassoon", "English Horn"],
+    correctAnswer: "Clarinet",
+}
